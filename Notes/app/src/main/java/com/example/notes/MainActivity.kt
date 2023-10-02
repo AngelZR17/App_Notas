994d2b19dd0057301165db25effbd8af1d5bec25
D4v1dD4v1d <david.pz.zv@gmail.com>
2023-10-02 17:13:13 -0600

    temas de colores agregados además de la mejora para las tatjetas de tareas

--- Notes/app/src/main/java/com/example/notes/MainActivity.kt
+++ Notes/app/src/main/java/com/example/notes/MainActivity.kt
@@ -15,20 +15,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.notes.model.Nota
 import com.example.notes.model.Tarea
-import com.example.notes.model.notasInfo
 import com.example.notes.model.tareasInfo
 import com.example.notes.ui.theme.NotesTheme
-import java.time.Instant
-import java.util.Date
 
 
 class MainActivity : ComponentActivity() {
@@ -96,7 +96,7 @@ fun notaCard(nota: Nota, modifier: Modifier=Modifier){
                     text = nota.nombre
                 )
                 Text(
-                    text = nota.descripcion
+                    text = nota.descripcion.substring(0,20)+"..."
                 )
                 Text(
                     text = nota.fecha.toString()
@@ -104,8 +104,7 @@ fun notaCard(nota: Nota, modifier: Modifier=Modifier){
             }
         }
     }
-}
-@Composable
+}@Composable
 fun taskCard(tarea: Tarea, modifier: Modifier=Modifier){
     Card (
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
@@ -119,41 +118,48 @@ fun taskCard(tarea: Tarea, modifier: Modifier=Modifier){
                 .padding(16.dp)
                 .sizeIn(minHeight = 72.dp)
         ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    text = tarea.nombre
-                )
-                Text(
-                    text = tarea.descripcion
-                )
-                Text(
-                    text = tarea.fecha.toString()
-                )
-                Text(
-                    text = tarea.realizada.toString()
-                )
+            val checked = remember { mutableStateOf(false)}
+            Column(modifier = Modifier.fillMaxWidth(0.9f)) {
+                Row {
+                    Text(
+                        text = tarea.nombre
+                    )
+                }
+                Row {
+                    Text(
+                        text = tarea.descripcion.substring(0,20)+"..."
+                    )
+                }
+                Row {
+                    Text(
+                        text = tarea.fecha.toString()
+                    )
+                }
+            }
+
+            Column(modifier = Modifier.fillMaxWidth(0.1f)){
+                Row {
+
+                }
+                Row {
+                    Checkbox(
+                        checked=checked.value,
+                        onCheckedChange = {isChecked ->checked.value=isChecked}
+                    )
+                }
+
             }
         }
-    }
-}
-/*
-@RequiresApi(Build.VERSION_CODES.O)
-@Preview(showBackground = true)
-@Composable
-fun ShowCard() {
-    NotesTheme {
-        val nota: Nota=notasInfo.notas[0];
-        notaCard(nota)
     }
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
-fun ShowTask() {
-    NotesTheme{
-        val tarea: Tarea=tareasInfo.tareas[0];
-        notaCard(tarea)
+fun ShowCard() {
+    NotesTheme {
+        val nota: Tarea=tareasInfo.tareas[0];
+        taskCard(nota)
     }
 }
-*/
+
