c4ae5e6d048d4213853f4a817a1783f73ad2a979
AngelDZR <S20120166@alumnos.itsur.edu.mx>
2023-10-02 19:09:57 -0600

    TopBar de la aplicacion

--- Notes/app/src/main/java/com/example/notes/MainActivity.kt
+++ Notes/app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.notes
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -13,16 +14,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.notes.model.Nota
@@ -104,13 +113,17 @@ fun notaCard(nota: Nota, modifier: Modifier=Modifier){
             }
         }
     }
-}@Composable
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun taskCard(tarea: Tarea, modifier: Modifier=Modifier){
+    Scaffold(
+        topBar = { TopBar() }){}
     Card (
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
-
-        modifier = modifier,
-
+        modifier = modifier.padding(top = 80.dp),
         ) {
         Row(
             modifier = Modifier
---
+++ Notes/app/src/main/java/com/example/notes/TopBar.kt
@@ -0,0 +1,66 @@
+package com.example.notes
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.notes.ui.theme.NotesTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(){
+    Box(modifier = Modifier
+        .height(80.dp)
+        .fillMaxSize()
+    ){
+        TopAppBar(
+            //modifier = Modifier.padding(top = 24.dp),
+            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
+            title = {
+                Text(stringResource(id = R.string.title), color = Color.White, fontWeight = FontWeight.Bold)
+            },
+            actions = {
+                TopBarActions()
+            }
+        )
+    }
+}
+
+@Composable
+fun TopBarActions(){
+    MenuAction()
+}
+@Composable
+fun MenuAction(){
+    val context = LocalContext.current
+    IconButton(onClick = {}
+    ){
+        Icon(imageVector = Icons.Filled.Menu, contentDescription = "menu_icon", tint = Color.White)
+    }
+}
+
+@Preview
+@Composable
+fun TopBarPreview(){
+    NotesTheme {
+        TopBar()
+    }
+}
