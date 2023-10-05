d185ca7456236f6bac74e581114d02eacd09bab4
brianml12 <s20120168@alumnos.itsur.edu.mx>
2023-10-04 20:24:13 -0600

    Notas

---
+++ Notas/app/src/main/java/com/brian_david_angel/notas/MainActivity.kt
@@ -0,0 +1,144 @@
+package com.brian_david_angel.notas
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.brian_david_angel.notas.data.DataNotes
+import com.brian_david_angel.notas.model.NotasPrincipal
+import com.brian_david_angel.notas.ui.theme.NotasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
+@Composable
+fun NotesAppBar(){
+    val context = LocalContext.current
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Notas") },
+                actions = {
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(Icons.Default.Search, contentDescription = "Buscar")
+                    }
+                }
+            )
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    val intent = Intent(context, AddNoteActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    context.startActivity(intent)
+                }
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Agregar")
+            }
+        },
+        content = {
+            ListNotas(contentPadding = it)
+        }
+    )
+}
+
+@Composable
+fun ListNotas(contentPadding: PaddingValues = PaddingValues(0.dp)){
+    val notas: List<NotasPrincipal> = DataNotes.notas
+    LazyColumn(
+        contentPadding = contentPadding
+    ){
+        items(notas) {
+                nota -> tarjetaNota(nota)
+        }
+    }
+}
+
+@Composable
+fun tarjetaNota(notasPrin: NotasPrincipal) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .sizeIn(minHeight = 72.dp)
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = notasPrin.titulo,
+                    style = MaterialTheme.typography.displaySmall
+                )
+                Text(
+                    text = notasPrin.descripcion,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Text(
+                    text = notasPrin.fecha,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NotasTheme {
+        NotesAppBar()
+    }
+}
---
+++ Notas/app/src/main/java/com/brian_david_angel/notas/data/DataNotes.kt
@@ -0,0 +1,18 @@
+package com.brian_david_angel.notas.data
+
+import com.brian_david_angel.notas.model.NotasPrincipal
+
+object DataNotes {
+    val notas = listOf(
+        NotasPrincipal(
+            titulo = "Nota1",
+            descripcion = "Esta es una nota",
+            fecha = "12"
+        ),
+        NotasPrincipal(
+            titulo = "Nota1",
+            descripcion = "Esta es una nota",
+            fecha = "12"
+        )
+    )
+}
