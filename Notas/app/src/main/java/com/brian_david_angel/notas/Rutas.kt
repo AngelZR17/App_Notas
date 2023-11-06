30454f7ec7e464c35bf89731e94cb7eb8b10f4a7
brianml12 <98165256+brianml12@users.noreply.github.com>
2023-11-06 10:31:46 -0600

    Delete Notas directory

--- Notas/app/src/main/java/com/brian_david_angel/notas/Rutas.kt
+++
@@ -1,6 +0,0 @@
-package com.brian_david_angel.notas
-
-sealed class Rutas(val ruta: String){
-    object HomeScreen: Rutas("home")
-    object NoteScreen: Rutas("note")
-}
--- Notas/app/src/main/java/com/brian_david_angel/notas/model/NotasPrincipal.kt
+++
@@ -1,9 +0,0 @@
-package com.brian_david_angel.notas.model
-
-import androidx.annotation.StringRes
-
-data class NotasPrincipal(
-    val titulo: String,
-    val descripcion: String,
-    val fecha: String
-)
